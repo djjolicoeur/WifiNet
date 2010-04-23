21bc2a65d0700b1ae1703a07e4d104bccdccc15b
Dan Jolicoeur <djjolicoeur@gmail.com>
2010-04-23 16:34:52 -0400

    retabbed

--- WifiNet.rb
+++ WifiNet.rb
@@ -3,40 +3,40 @@
 require 'rubygems'
 
 
-	
+  
 class WifiNet
-	attr_accessor :essid, :quality, :key
+  attr_accessor :essid, :quality, :key
 
-	def initialize(iface)
-		system("ifconfig #{iface} up")
-		essidStack = []
-		qualityStack = []
-		keyStack = []
-		p = IO.popen("iwlist #{iface} scanning")
-		data = p.readlines
-		data.each do |d|
-			string = d
-			split = d.split(":")
-			if split[0].strip.downcase.include?("essid")
-				essidStack.push(split[1].strip)
-			else if  split[0].strip.downcase.include?("quality")
-				qualityStack.push(split[1].strip)
-			else if split[0].strip.capitalize.include?("key")
-				keyStack.push(split[1].strip)
-			end
-		end
-
-
-
-	
+  def initialize(iface)
+    system("ifconfig #{iface} up")
+    essidStack = []
+    qualityStack = []
+    keyStack = []
+    p = IO.popen("iwlist #{iface} scanning")
+    data = p.readlines
+    data.each do |d|
+      string = d
+      split = d.split(":")
+      if split[0].strip.downcase.include?("essid")
+        essidStack.push(split[1].strip)
+      else if  split[0].strip.downcase.include?("quality")
+        qualityStack.push(split[1].strip)
+      else if split[0].strip.capitalize.include?("key")
+        keyStack.push(split[1].strip)
+      end
+    end
 
 
 
-		
-	end
+  
 
-	def connect()
-		
+
+
+    
+  end
+
+  def connect()
+    
 end
 
 
