51abc82a347f8ee74c624f088a4d2c22aedbfeaf
Dan Jolicoeur <djjolicoeur@gmail.com>
2010-04-26 12:13:25 -0400

    still hackish, but connects to the strongest unsecured network available.

--- WifiNet.rb
+++ WifiNet.rb
@@ -5,64 +5,87 @@ require 'rubygems'
 
   
 class WifiNet
-  attr_accessor :essid, :quality, :key
+  attr_accessor :essid, :quality, :key, :iface
+  
 
   def initialize(iface)
-    system("ifconfig #{iface} up")
+    @iface = iface
+    t = system("ifconfig #{iface} up")
+    if t == false
+      puts "interface error: exiting"
+      exit 
+    end
     essidStack = []
     qualityStack = []
     keyStack = []
     p = IO.popen("iwlist #{iface} scanning")
-    data = p.readlines
+    data = p.readlines  
     data.each do |d|
       string = d
-      split = d.split(":")
-      if split[0].strip.downcase.include?("essid")
-        essidStack.push(split[1].strip)
-      else if  split[0].strip.downcase.include?("quality")
-        qualityStack.push(split[1].strip)
-      else if split[0].strip.capitalize.include?("key")
-        keyStack.push(split[1].strip)
+      
+      if string.include?(":")
+        split = string.split(":")
+      elsif string.include?("=")
+        split = string.split("=")
+      end
+
+
+
+
+      if split != nil and split.size > 1 
+        if split[0].strip.downcase.include?("essid")
+          essidStack.push(split[1].strip)
+        elsif  split[0].strip.downcase.include?("quality")
+          qualityStack.push(split[1].strip)
+        elsif split[0].strip.downcase.include?("key")
+          keyStack.push(split[1].strip)
+        end
       end
     end
-    
+
+    if essidStack.size < 1
+      puts "No available unencrypted networks: exiting"
+      exit
+    end
+        
+
     q = 0
     while essidStack.size() != 0
       e = essidStack.pop
       rawQual = qualityStack.pop
       k = keyStack.pop
 
+      e = e.gsub('"', '')
       split = rawQual.split("/")
+      split[1] = split[1][0..2]
+      newQ = split[0].to_f / split[1].to_f
       
-      newQ = split[0].to_i / split[1].to_i
-      
-      if newQ > q and key.downcase == "off"
+      if newQ > q and k.downcase == "off" and e != ""
         q = newQ
         @essid = e
         @key = k
         @quality = q
       end
-
-
-
-
-
-  
-
-
-
+    end
     
   end
-
+        
   def connect()
+    system("dhcpcd -k #{@iface}")
+    system("iwconfig #{@iface} essid \"#{@essid}\" key #{@key}")
+    system("dhcpcd #{@iface}")
+  end
     
-end
+
+end    
 
 
-net = WifiNet.new("test", "64/76","off")
+net = WifiNet.new("wlan1")
 
 puts net.essid
 puts net.quality
 puts net.key
+
+net.connect()
 
 
